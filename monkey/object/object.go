9df5476f86014828ba9a609827e65206a7353aa6
kantentenka <yutakimata@gmail.com>
2022-04-07 07:18:45 +0900

    3.7

--- monkey/object/object.go
+++ monkey/object/object.go
@@ -2,7 +2,9 @@
 
 package object
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type ObjectType string
 
@@ -10,6 +12,8 @@ const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
 	NULL_OBJ    = "NULL"
+
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 type Object interface {
@@ -35,3 +39,10 @@ type Null struct{}
 
 func (n *Null) Type() ObjectType { return NULL_OBJ }
 func (n *Null) Inspect() string  { return "null" }
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
